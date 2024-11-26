ae4f8142894b5a8c910e3bad927fa73b917da422
MlleLondon <garancedugimont@gmail.com>
2024-11-26 09:05:28 +0100

    Changements fichier home

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 
 @Component({
@@ -8,6 +8,27 @@ import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/stan
   standalone: true,
   imports: [IonHeader, IonToolbar, IonTitle, IonContent],
 })
-export class HomePage {
-  constructor() {}
+export class HomePage implements OnInit {
+  //Liste carroussel images
+  images: string[] = [
+    'assets/Bateaux/chalutierBreton1.jpg', 
+    'assets/Bateaux/chalutierBreton2.jpg',
+    'assets/Bateaux/chalutierBreton3.jpg',
+    'assets/Bateaux/chalutierBreton4.jpg'
+  ];
+  currentImage: string = '';
+  private currentIndex: number = 0;
+
+  ngOnInit() {
+    this.startCarousel();
+  }
+
+  startCarousel() {
+    //Switch d'images du carroussel toutes les 4 secondes
+    this.currentImage = this.images[this.currentIndex];
+    setInterval(() => {
+      this.currentIndex = (this.currentIndex + 1) % this.images.length;
+      this.currentImage = this.images[this.currentIndex];
+    }, 4000); // Change toutes les 4 secondes
+  }
 }
