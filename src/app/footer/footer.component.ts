77e8d6c5257cedc13592f49a47a913d548f161b8
MlleLondon <garancedugimont@gmail.com>
2024-11-28 09:32:26 +0100

    Changements*

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,14 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
+  standalone: true,
+  imports: [CommonModule]
 })
-export class FooterComponent  implements OnInit {
+export class FooterComponent {
+  // Variable pour vérifier si l'on est sur la page d'accueil
+  showLogo: boolean = true;
 
-  constructor() { }
+  constructor(private router: Router) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    // Vérifie l'URL actuelle pour ne pas afficher le logo sur la page d'accueil
+    this.router.events.subscribe(() => {
+      const currentUrl = this.router.url;
+      this.showLogo = currentUrl !== '/home'; // Modifie en fonction de ta route d'accueil
+    });
+  }
 
+  // Fonction de navigation vers la page d'accueil
+  goHome() {
+    this.router.navigate(['/home']); // Modifie en fonction de la route d'accueil
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,14 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle } from "@ionic/angular/standalone";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
+  standalone: true,
+  imports: [IonTitle, IonToolbar, IonHeader]
 })
-export class HeaderComponent  implements OnInit {
-
-  constructor() { }
+export class HeaderComponent implements OnInit {
+  constructor() {}
 
   ngOnInit() {}
-
 }
